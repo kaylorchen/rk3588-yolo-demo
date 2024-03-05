ad1bf6136c831eaf94a198d8511a967ab272f883
kaylorchen <kaylor.chen@qq.com>
2024-03-05 23:28:01 +0800

    添加线程池，但是有问题

--- src/yolov8/main.cpp
+++ src/yolov8/main.cpp
@@ -2,24 +2,31 @@
 #include "yolov8.h"
 #include "image_process.h"
 #include "kaylordut/log/logger.h"
+#include "threadpool.h"
+
+void run(Yolov8 &yolov_8, ImageProcess &image_process, std::shared_ptr<cv::Mat> origin, cv::Mat rgb, const letterbox_t &letter_box){
+  object_detect_result_list od_results;
+  yolov_8.Inference(rgb.ptr(),  &od_results, letter_box); image_process.ImagePostProcess(*origin, od_results);
+  cv::imshow("Video", *origin);
+  cv::waitKey(1);
+}
+
 int main(int argc, char *agrv[]) {
-  Yolov8 yolov_8("./yolov8n.rknn");
+  Yolov8 yolov_8(agrv[1]);
 //  Yolov8 yolov_8("./yolov8s-seg.rknn");
-  VideoFile video_file(agrv[1]);
+  VideoFile video_file(agrv[2]);
 //    video_file.Display(125, 640);
   ImageProcess image_process(video_file.get_frame_width(), video_file.get_frame_height(), 640);
   auto original_img = video_file.GetNextFrame();
   auto convert_img = image_process.Convert(*original_img);
   cv::Mat rgb_img = cv::Mat::zeros(640, 640, convert_img->type());
   cv::namedWindow("Video", cv::WINDOW_AUTOSIZE);
+  ThreadPool pool(1);
   while (convert_img != nullptr) {
     cv::cvtColor(*convert_img, rgb_img, cv::COLOR_BGR2RGB);
-    object_detect_result_list od_results;
-    yolov_8.Inference(rgb_img.ptr(),  &od_results, image_process.get_letter_box());
 
-    image_process.ImagePostProcess(*original_img, od_results);
-    cv::imshow("Video", *original_img);
-    cv::waitKey(1);
+    pool.enqueue([&]{ run(yolov_8, image_process, std::move(original_img), rgb_img, image_process.get_letter_box());});
+
     original_img = video_file.GetNextFrame();
     convert_img = image_process.Convert(*original_img);
   }
