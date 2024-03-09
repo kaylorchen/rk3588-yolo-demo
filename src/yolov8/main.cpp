16aea4537f185c3a7afb42b0c9ecddb3a54af8f8
kaylorchen <kaylor.chen@qq.com>
2024-03-09 20:31:14 +0800

    使用定时帧数计算

--- src/yolov8/main.cpp
+++ src/yolov8/main.cpp
@@ -3,10 +3,13 @@
 #include "kaylordut/time/time_duration.h"
 #include "rknn_pool.h"
 #include "videofile.h"
+#include "kaylordut/time/run_once.h"
 
 int main(int argc, char *argv[]) {
-  auto rknn_pool = std::make_unique<RknnPool>(argv[1], 4);
+  auto rknn_pool = std::make_unique<RknnPool>(argv[1], std::atoi(argv[3]));
   VideoFile video_file(argv[2]);
+  int frame_rate = std::atoi(argv[4]);
+  int delay = 1000 / frame_rate;
   ImageProcess image_process(video_file.get_frame_width(),
                              video_file.get_frame_height(), 640);
   std::unique_ptr<cv::Mat> image;
@@ -17,27 +20,29 @@ int main(int argc, char *argv[]) {
   static int image_res_count = 0;
   TimeDuration time_duration;
   do {
-    running_flag = 0;
-    image = video_file.GetNextFrame();
-    if (image != nullptr) {
-      rknn_pool->AddInferenceTask(std::move(image), image_process);
-      running_flag |= 0x01;
-      image_count++;
-    }
-    image_res = rknn_pool->GetImageResultFromQueue();
-    if (image_res != nullptr) {
-      cv::imshow("Video", *image_res);
-      image_res_count++;
-      KAYLORDUT_LOG_INFO("image count = {}, image res count = {}", image_count,
-                         image_res_count);
-      cv::waitKey(1);
-      running_flag |= 0x10;
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    auto func = [&] {
+      running_flag = 0;
+      image = video_file.GetNextFrame();
+      if (image != nullptr) {
+        rknn_pool->AddInferenceTask(std::move(image), image_process);
+        running_flag |= 0x01;
+        image_count++;
+      }
+      image_res = rknn_pool->GetImageResultFromQueue();
+      if (image_res != nullptr) {
+        cv::imshow("Video", *image_res);
+        image_res_count++;
+        KAYLORDUT_LOG_INFO("image count = {}, image res count = {}, delta = {}", image_count,
+                           image_res_count, image_count - image_res_count);
+        cv::waitKey(1);
+        running_flag |= 0x10;
+      }
+    };
+    run_once_with_delay(func, std::chrono::milliseconds(delay));
   }
-  // 读取图像或者获取结果非空 线程池里还有任务 推理的结果没有等于输入图像数
+    // 读取图像或者获取结果非空 线程池里还有任务 推理的结果没有等于输入图像数
   while (running_flag || rknn_pool->GetTasksSize() ||
-         image_count > image_res_count);
+      image_count > image_res_count);
   auto time = std::chrono::duration_cast<std::chrono::milliseconds>(
       time_duration.DurationSinceLastTime());
   double fps = image_res_count * 1000.0 / time.count();
